883f62f45798ba0872beaa77c95e6a82034831c2
ALEXANDRE HENRIQUE SILVA <145609944+ProfAlexandrePC@users.noreply.github.com>
2023-10-31 16:58:15 -0300

    adicionando arquivos html, js, css.

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(300, 300);
+    }
+    function draw() {
+    background("white");
+    fill("black");
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    let y = "Está";
+    let z = "Tudo";
+    let x = "Embaralhado";
+    text(x, 150, 150);
+    text(y, 200, 50);
+    text(z, 75, 100);
+    }
